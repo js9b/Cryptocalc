499e48ddb8525e64a78e7867b677a1caac6203c0
js9b <51540049+js9b@users.noreply.github.com>
2019-06-09 21:50:44 +0200

    Start

---
+++ server.js
@@ -0,0 +1,40 @@
+const rp = require('request-promise');
+var express = require('express');
+var app = express();
+var path = require('path');
+let currency
+
+const requestOptions = {
+  method: 'GET',
+  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+  qs: {
+    'start': '1',
+    'limit': '200',
+    'convert': 'USD'
+  },
+  headers: {
+    'X-CMC_PRO_API_KEY': '5b3f058a-fa58-4c54-a7cc-d5c9be76cf0d'
+  },
+  json: true,
+  gzip: true
+};
+
+rp(requestOptions).then(response => {
+  currency = response;
+  console.log("Works!");
+
+}).catch((err) => {
+  console.log('API call error:', err.message);
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.get('/test', function (req, res, next) {
+  res.json({ currency });
+});
+
+app.listen(8000, function () {
+  console.log("such")
+})
