f103b270b87d17604263221cd87fd0a82b87804d
js9b <51540049+js9b@users.noreply.github.com>
2020-06-16 00:34:44 +0200

    test2

--- server.js
+++ server.js
@@ -53,7 +53,8 @@ if(process.env.NODE_ENV === 'production') {
 }*/
 //////////////////////////
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  //res.sendFile(path.join(__dirname + '/index.html'));
+  console.log("work?")
 })
 //app.get('/test', function (req, res, next) {
 //  res.json({ currency });
