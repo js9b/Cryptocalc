391b881d276f1905472b8878d13bed2dd1d967db
js9b <51540049+js9b@users.noreply.github.com>
2020-09-04 21:37:18 +0200

    v5

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(express.static(__dirname + '/'));
 //app.use(cors({origin: 'https://cryptocalq.herokuapp.com:4000'}));
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://cryptocalq.herokuapp.com:4000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://cryptocalq.herokuapp.com/test:4000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
