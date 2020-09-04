0d334c48e3c91ec27eb9657597972971258ee341
js9b <51540049+js9b@users.noreply.github.com>
2020-09-04 21:32:48 +0200

    v3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const rp = require('request-promise');
 var express = require('express');
 var app = express();
 var path = require('path');
-var cors = require('cors');
+//var cors = require('cors');
 
 const process = require('process');
 
@@ -55,15 +55,15 @@ if(process.env.NODE_ENV === 'production') {
 }*/
 //////////////////////////
 app.use(express.static(__dirname + '/'));
-app.use(cors({origin: 'https://cryptocalq.herokuapp.com:4000'}));
+//app.use(cors({origin: 'https://cryptocalq.herokuapp.com:4000'}));
 
-//app.use(function (req, res, next) {
-    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    //res.setHeader('Access-Control-Allow-Credentials', true);
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://cryptocalq.herokuapp.com:4000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     //next();
-//});
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
